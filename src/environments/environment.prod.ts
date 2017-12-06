524ae9e804f8bcf2886af9203b6e635c4c3c35ec
Alec Dilanchian <aldilanc@microsoft.com>
2017-12-06 15:53:01 -0600

    Adding base angular project for gh-pages

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  pusher: {
+      key: '0d1732e32f0dea340a96',
+      cluster: 'us2',
+      encrypted: true,
+      channel: 'my-resume',
+      event: 'update-resume'
+  },
+  ngrok_url: '//realtimeresume.azurewebsites.net/'
+};
